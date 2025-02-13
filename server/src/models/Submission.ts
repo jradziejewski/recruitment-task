f9ee000965b833f0922778eaa78b0511ef2b062d
jradziejewski <192462@student.ue.wroc.pl>
2025-02-14 00:35:03 +0100

    create submissions model and migration

--- server/src/models/Submission.ts
+++ server/src/models/Submission.ts
@@ -1,13 +1,29 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { v4 as uuidv4 } from "uuid";
 
 @Entity()
 export class Submission {
   @PrimaryKey({ type: "uuid" })
-  id!: string;
+  id: string = uuidv4();
 
   @Property({ type: "string" })
   firstName!: string;
 
   @Property({ type: "string" })
+  lastName!: string;
+
+  @Property({ type: "string" })
+  company!: string;
+
+  @Property({ type: "string" })
+  phoneNumber!: string;
+
+  @Property({ type: "string" })
   email!: string;
+
+  @Property({ type: "string" })
+  message!: string;
+
+  @Property({ type: "datetime", defaultRaw: "CURRENT_TIMESTAMP" })
+  createdAt!: Date | null;
 }
